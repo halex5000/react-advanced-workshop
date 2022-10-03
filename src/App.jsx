df76b6e793f66544b2dc9da5e39f5fc0a1dedb98
Alex Hardman <alexhardman@launchdarkly.com>
2022-10-03 15:46:51 -0400

    adding .env

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
+import {useTheme, ThemeProvider, createTheme} from '@mui/material/styles';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
 
-function App() {
-  const [count, setCount] = useState(0)
+const ColorModeContext = React.createContext({toggleColorMode() {}});
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+function MyApp() {
+	const theme = useTheme();
+	const colorMode = React.useContext(ColorModeContext);
+	return (
+		<Box
+			sx={{
+				display: 'flex',
+				width: '100%',
+				alignItems: 'center',
+				justifyContent: 'center',
+				bgcolor: 'background.default',
+				color: 'text.primary',
+				borderRadius: 1,
+				p: 3,
+			}}
+		>
+			{theme.palette.mode} mode
+			<IconButton
+				sx={{ml: 1}}
+				color="inherit"
+				onClick={colorMode.toggleColorMode}
+			>
+				{theme.palette.mode === 'dark' ? (
+					<Brightness7Icon />
+				) : (
+					<Brightness4Icon />
+				)}
+			</IconButton>
+		</Box>
+	);
 }
 
-export default App
+export default function ToggleColorMode() {
+	const [mode, setMode] = (React.useState < 'light') | ('dark' > 'light');
+	const colorMode = React.useMemo(
+		() => ({
+			toggleColorMode() {
+				setMode((previousMode) =>
+					previousMode === 'light' ? 'dark' : 'light',
+				);
+			},
+		}),
+		[],
+	);
+
+	const theme = React.useMemo(
+		() =>
+			createTheme({
+				palette: {
+					mode,
+				},
+			}),
+		[mode],
+	);
+
+	return (
+		<ColorModeContext.Provider value={colorMode}>
+			<ThemeProvider theme={theme}>
+				<MyApp />
+			</ThemeProvider>
+		</ColorModeContext.Provider>
+	);
+}
