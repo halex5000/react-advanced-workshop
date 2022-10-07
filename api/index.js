32970f758feecbe886182efcafe20a157b582aee
Peter McCarron <pcmccarron@gmail.com>
2022-10-07 12:46:38 -0600

    updated index.js file

--- api/index.js
+++ api/index.js
@@ -2,45 +2,47 @@ require('dotenv').config();
 const express = require("express");
 const app = express ();
 const cors = require("cors");
-const LaunchDarkly = require ('launchdarkly-node-server-sdk'); //We need to load the LaunchDarkly Node Server-side SDK
+//We need to load the LaunchDarkly Node Server-side SDK
 app.use (cors())
 app.use (express.json());
 const reload = require("reload");
 const http = require('http');
 
 //Initialize LaunchDarkly
-client = LaunchDarkly.init(process.env.LAUNCHDARKLY_SDK_KEY)
+/* TODO: Add the client initialization function */
 
 
 async function init () {
     //wait for initialization before moving forward
-    try {
+   /* try {
         await client.waitForInitialization();
     }
     catch (err) {
     }
-
+*/
 
 //Create pool
 const { Pool } = require("pg");
 const pool = new Pool({
-  user: 'postgres', //update with your username if building locally
-  host: 'db', //change to localhost if building locally
+  user: 'pcmccarron', //update with your username if building locally
+  host: 'localhost', //change to localhost if building locally
   database: 'demo', //update with correct database name if building locally
-  password: 'postgres_password', //update accordingly 
+  //password: 'postgres_password', //update accordingly 
   port: 5432
 });
 
-const apiFlag = await client.variation(
-    "apiFlag",
-    { key: "anonymous" },
-    false
-  );
 
-if (apiFlag) {
     
 //Routes
 app.post("/api", async(req, res) => {
+    /* const apiFlag = await client.variation(
+        "apiFlag",
+        { key: "anonymous" },
+        false
+      );
+    */
+   let apiFlag = false;
+    if (apiFlag) {
  try {
     const { description } = req.body;
     const newTodo = await pool.query("INSERT INTO todos (description) VALUES($1) RETURNING *",
@@ -50,9 +52,17 @@ app.post("/api", async(req, res) => {
  } catch (err) {
      console.error(err.message);
  }
+}
+ else {
+    res.status(404).send("bad connection")
+}
 });
 
 app.get("/api", async(req, res) => {
+ /* grab the flag values
+    */
+      let apiFlag = false;
+    if (apiFlag) {
     try {
         const allTodos = await pool.query("SELECT * FROM todos");
     res.json(allTodos.rows);
@@ -60,20 +70,35 @@ app.get("/api", async(req, res) => {
     catch (err) {
         console.error(err.message)
     }
+}
+    else {
+        res.status(404).send("bad connection")
+    }
 });
 
 app.get("/api/:id", async (req, res) => {
+ /* grab the flag values
+    */
+      let apiFlag = false;
+    if (apiFlag) {
     try {
        const { id } = req.params;
        const todo = await pool.query("SELECT * FROM todos WHERE todo_id = $1", [id])
 
        res.json(todos.rows[0]);
-
    } catch (err) {
        console.error(err.message)
    }
+}
+else {
+    res.status(404).send("bad connection")
+}
 });
 app.put("/api/:id", async (req, res) => {
+ /* grab the flag values
+    */
+      let apiFlag = false;
+    if (apiFlag) {
     try {
         const { id } = req.params;
         const { description } = req.body;
@@ -84,9 +109,17 @@ app.put("/api/:id", async (req, res) => {
     } catch (err) {
         console.error(err.message)
     }
+}
+    else {
+        res.status(404).send("bad connection")
+    }
 });
 
 app.delete("/api/:id", async (req, res) => {
+/* grab the flag values
+    */
+      let apiFlag = false;
+    if (apiFlag) {
 try {
     const { id } = req.params;
     const deleteTodo = await pool.query ("DELETE FROM todos WHERE todo_id = $1", 
@@ -96,21 +129,12 @@ try {
 } catch (err) {
     console.error(err.message)
 }
-});
 }
 else {
-    app.post("/wrong_value", async(req, res) => {
-        try {
-           const { description } = req.body;
-           const newTodo = await pool.query("INSERT INTO todos (description) VALUES($1) RETURNING *",
-           [description]
-           );
-          res.json(newTodo.rows[0]);
-        } catch (err) {
-            console.error(err.message);
-        }
-       });
+    res.status(404).send("bad connection")
 }
+});
+
 
 app.set("port", 5000)
 const server = http.createServer(app);
@@ -119,9 +143,13 @@ server.listen(app.get("port"), cors(), function () {
     console.log('The API server is now live')
 })
 
+
+// We can also detect flag changes, feel free to uncomment this section to see this in action
+/*
 reload(app).then((reloadReturned) => {
 client.on('update:apiFlag', () => {
-            reloadReturned.reload();
+        console.log('the flag has changed'); 
+        reloadReturned.reload();
         })
     })
     .catch(function (err) {
@@ -130,6 +158,7 @@ client.on('update:apiFlag', () => {
           err
         );
       });
+*/
 }
 
 init();
