433023a320514dee9e79f1943ce109e1b7e6d406
Peter M <36747421+pcmccarron@users.noreply.github.com>
2022-10-13 09:49:49 -0600

    removing unnecessary code

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,7 @@ import { asyncWithLDProvider } from 'launchdarkly-react-client-sdk';
 	const LDProvider = await asyncWithLDProvider({
 		clientSideID: import.meta.env.VITE_LD_CLIENT_KEY,  
 		user: {
-			"key": "anonymous",
-			"name": "John Doe"  
+			"key": "anonymous", 
 		}
 	})
 
